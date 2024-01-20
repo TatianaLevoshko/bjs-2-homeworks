a028fb463e60de9ce3b662b37a5e49b41480890a
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-01-20 04:24:11 +0300

    Реализовано первое задание функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,30 @@
+
+"use strict";
+const arr = [
+  [10, 6, 23],
+  [3, 48, 22],
+  [11, 8, 6],
+];
+
 function getArrayParams(...arr) {
+  if (!arr.length) {
+		return 0;
+  }
+ 
+
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const sum = arr.reduce((a, b) => (a + b)) / arr.length;
+  const avg = number((sum).toFixed(2));
+  
 
   return { min: min, max: max, avg: avg };
 }
+
+
+
+
+
 
 function summElementsWorker(...arr) {
 
