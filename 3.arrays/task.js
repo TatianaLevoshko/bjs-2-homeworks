0784154b19f98adc913b34c19a1fbbd420882765
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-02-01 01:56:18 +0300

    решение задача1 массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
+
+
 
 function getUsersNamesInAgeRange(users, gender) {
   
