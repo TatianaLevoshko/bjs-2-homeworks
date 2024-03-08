3579726a0e257c59a29309a2ca4b39816a55c6ed
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-03-08 14:24:37 +0300

    1

--- .history/6.exception-closure/task_20240308142241.js
+++ .history/6.exception-closure/task_20240308142241.js
@@ -2,7 +2,7 @@
 
 function parseCount(value) {
 	let num = Number.parseFloat(value)  			//парсинг
-	if (isNaN(num)) {								//проверка на значение
+	if (is(num)) {								//проверка на значение
 		throw new Error('Недопустимое значение')	//выброс ошибки
 	}
 	return num;										//возврат результата
